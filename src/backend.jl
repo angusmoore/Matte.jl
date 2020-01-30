4ccc7f8b8574a565cd747c3e2c8ccd05f1b204ed
Angus Moore <18376371+angusmoore@users.noreply.github.com>
2020-01-30 17:39:02 +1100

    Helpful error message if app has a server-side function that includes a non-existent input
    Closes #14

--- src/backend.jl
+++ src/backend.jl
@@ -4,13 +4,24 @@ function handle_request(id, input_dict, server_module)
     names = argument_names(fn)
     args = ()
     for argname in names
-        args = (args..., input_dict[string(argname)])
+        try
+            args = (args..., input_dict[string(argname)])
+        catch e
+            @error "Bad Server module configuration: Server-side function `$id` takes `$argname` as an input, but there is no UI element with that id"
+            msg = sprint(showerror, e)
+            if typeof(e) <: KeyError
+                return Genie.Renderer.Json.json(Dict("matte_error_msg" => "Bad Server module configuration: Server-side function `$id` takes `$argname` as an input, but there is no UI element with that id"))
+            else
+                return Genie.Renderer.Json.json(Dict("matte_error_msg" => msg))
+            end
+        end
     end
     try
         response = fn(args...)
         Genie.Renderer.Json.json(Dict(id => response))
     catch e
         @error e
-        Genie.Renderer.Json.json(Dict("matte_error_msg" => "$(typeof(e)): $(e.msg)"))
+        msg = sprint(showerror, e)
+        Genie.Renderer.Json.json(Dict("matte_error_msg" => msg))
     end
 end
