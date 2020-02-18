1b50fac4358faa83b9cb4dcd6d384b2c97e36f47
angusmoore <18376371+angusmoore@users.noreply.github.com>
2020-02-18 19:02:36 +1100

    Update tests to include all examples

--- test/test-examples.jl
+++ test/test-examples.jl
@@ -12,6 +12,46 @@ cd("sandpit")
     cd("../")
 end
 
+@testset "bad_long_running" begin
+    matte_example("bad_long_running", "bad_long_running", activate = false)
+    include(joinpath(pwd(), "app.jl"))
+    run_app(BadLongRunningExample, async = true)
+    res = HTTP.request("GET", "http://localhost:8000/")
+    @test res.status == 200
+    stop_app()
+    cd("../")
+end
+
+@testset "better_long_running" begin
+    matte_example("better_long_running", "better_long_running", activate = false)
+    include(joinpath(pwd(), "app.jl"))
+    run_app(BetterLongRunningExample, async = true)
+    res = HTTP.request("GET", "http://localhost:8000/")
+    @test res.status == 200
+    stop_app()
+    cd("../")
+end
+
+@testset "blank" begin
+    new_matte_app("blank", activate = false)
+    include(joinpath(pwd(), "app.jl"))
+    run_app(NewMatteApp, async = true)
+    res = HTTP.request("GET", "http://localhost:8000/")
+    @test res.status == 200
+    stop_app()
+    cd("../")
+end
+
+@testset "broken_sideeffect" begin
+    matte_example("broken_sideeffect", "broken_sideeffect", activate = false)
+    include(joinpath(pwd(), "app.jl"))
+    run_app(BrokenSideEffect, async = true)
+    res = HTTP.request("GET", "http://localhost:8000/")
+    @test res.status == 200
+    stop_app()
+    cd("../")
+end
+
 @testset "dynamic_select" begin
     matte_example("dynamic_select", "dynamic_select", activate = false)
     include(joinpath(pwd(), "app.jl"))
@@ -26,6 +66,16 @@ end
     matte_example("hello_world", "hello_world", activate = false)
     include(joinpath(pwd(), "app.jl"))
     run_app(HelloWorldApp, async = true)
+    res = HTTP.request("GET", "http://localhost:8000/")
+    @test res.status == 200
+    stop_app()
+    cd("../")
+end
+
+@testset "iris" begin
+    matte_example("iris", "iris", activate = false)
+    include(joinpath(pwd(), "app.jl"))
+    run_app(IrisApp, async = true)
     res = HTTP.request("GET", "http://localhost:8000/")
     @test res.status == 200
     stop_app()
